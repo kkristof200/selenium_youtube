669470af12fdfa538723e4880370c95b12831f5c
Kovács Kristóf-Attila <kovacskristof200@gmail.com>
2021-02-04 18:05:44 +0200

    Update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,4 +6,4 @@ python3 -m pip install -U kstopit==0.0.10
 python3 -m pip install -U kyoutubescraper==0.0.2
 python3 -m pip install -U selenium==3.141.0
 python3 -m pip install -U selenium-firefox==0.5.36
-python3 -m pip install -U selenium-uploader-account==0.0.3
+python3 -m pip install -U selenium-uploader-account==0.0.5
