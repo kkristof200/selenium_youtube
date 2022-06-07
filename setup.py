3ffa485adb3bf5ecdc76f7e67d4405dacd1900bd
kkristof200 <kovacskristof200@gmail.com>
2022-06-07 15:38:50 +0300

    Update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setuptools.setup(
     name='selenium_youtube',
-    version='2.0.20',
+    version='2.0.21',
     author="Kovács Kristóf-Attila & Péntek Zsolt",
     description='selenium_youtube',
     long_description=long_description,
